d3a86a0506bb96e011daada7afa92af45bb8cee7
gushakov <Georgy.Ushakov@unil.ch>
2019-10-15 22:10:29 +0200

    removed unused imports

--- axon-zeebe-app/src/main/java/com/github/axonzeebe/config/SwaggerConfig.java
+++ axon-zeebe-app/src/main/java/com/github/axonzeebe/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.github.axonzeebe.config;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
